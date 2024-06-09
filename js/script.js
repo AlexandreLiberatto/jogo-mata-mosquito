669c821486c2c0b2a24a28ae800abd098545b53e
unknown <alexandreliberatto@gmail.com>
2024-06-09 14:11:43 -0300

    Ajustabdo bacgroud

--- js/script.js
+++ js/script.js
@@ -9,22 +9,6 @@ const ajustaTamanhoDoPalcoJogo = () => {
 
 ajustaTamanhoDoPalcoJogo()
 
-let posicaoX = Math.floor(Math.random() * largura) - 90
-let posicaoY = Math.floor(Math.random() * altura) - 90
-
-posicaoX = posicaoX < 0 ? 0 : posicaoX
-posicaoY = posicaoY < 0 ? 0 : posicaoY
-
-/* ====criar elemento html==== */
-let mosquito = document.createElement('img')
-mosquito.src = 'imagens/mosquito.png'
-mosquito.className = 'mosquito1'
-mosquito.style.left = posicaoX + 'px'
-mosquito.style.top = posicaoY + 'px'
-mosquito.style.position = 'absolute'
-
-document.body.appendChild(mosquito)
-
 const tamanhoAleatorio = () => {
     let classe = Math.floor(Math.random() * 3)
 
@@ -38,4 +22,47 @@ const tamanhoAleatorio = () => {
     }
 }
 
-tamanhoAleatorio()
+
+const ladoAleatorio = () => {
+    let classe = Math.floor(Math.random() * 2)
+
+    switch(classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+        
+    }
+}
+
+
+function posicaoRandomica () {
+
+    //Remover mosquito caso exista
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+    }
+
+    let posicaoX = Math.floor(Math.random() * largura) - 90
+    let posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    /* ====criar elemento html==== */
+    let mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+
+    document.body.appendChild(mosquito)
+
+}
+
+
+setInterval(function(){
+    posicaoRandomica()
+}, 1000)
